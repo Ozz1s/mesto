cdb219b84ca065047f8dc6e9f6acfb72f8e4f0f0
Islam Kabulzhanov <ozzisgo@yandex.ru>
2023-05-13 00:47:45 +0300

    fix: удаление не нужных файлов

--- scripts/validatorSettings.js
+++
@@ -1,8 +0,0 @@
-// export const config = {
-//     formSelector: '.popup__form',
-//     inputSelector: '.popup__name',
-//     submitButtonSelector: '.popup__save-btn',
-//     inactiveButtonClass: 'popup__button-inactive',
-//     inputErrorClass: 'popup__input_type_error',
-//     errorClass: 'popup__input-error_active'
-// };
